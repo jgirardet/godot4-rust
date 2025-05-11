2719f6dd863d3b999deb254531a22d8f319e71d0
jgirardet <ijkl@netc.fr>
2025-05-11 02:10:15 +0200

    fix update

--- src/godot/godotManager.ts
+++ src/godot/godotManager.ts
@@ -1,33 +1,45 @@
 import { glob } from "glob";
 import { FullPathDir, FullPathFile } from "../types";
 import { GodotScene } from "./godotScene";
-import { GodotPath } from "./godotPath";
+import { GodotPath, gp } from "./godotPath";
 import { getGodotProjectDir } from "./godotUtils";
 import { Worker } from "worker_threads";
 import { availableParallelism } from "os";
+import path from "path";
 
 export class GodotManager {
   dependencies: Map<string, Set<string>> = new Map();
   _godotProjectFile: FullPathFile;
   _godotProjectDir: FullPathDir;
   scenes: Map<string, GodotScene> = new Map();
+  lastUpdate: string[] = [];
 
   constructor(godotProjectFile: FullPathFile) {
     this._godotProjectFile = godotProjectFile;
     this._godotProjectDir = getGodotProjectDir(godotProjectFile);
   }
 
-  reset() {
+  async reset() {
     this.scenes.clear();
     this.dependencies.clear();
+    await this.load();
   }
 
   async load(nbWorker?: number) {
-    for (const bunch of await this._loadTscns(nbWorker)) {
+    let files = await glob("**/*.tscn", {
+      absolute: true,
+      cwd: this._godotProjectDir,
+      nodir: true,
+    });
+    await this.addScenes(files, nbWorker);
+  }
+
+  async addScenes(files: string[], nbWorker?: number) {
+    for (const bunch of await this._loadTscns(files, nbWorker)) {
       for (const ghostScene of bunch) {
         // js worken make loosing getter and other, need to redo the object
         const scene = new GodotScene(
-          new GodotPath(ghostScene._path._base),
+          gp(ghostScene._path._base),
           ghostScene._gdscene
         );
         for (const dep of scene.depedencies) {
@@ -38,14 +50,47 @@ export class GodotManager {
     }
   }
 
-  async _loadTscns(nbWorker?: number): Promise<GodotScene[][]> {
-    let files = await glob("**/*.tscn", {
-      absolute: true,
-      cwd: this._godotProjectDir,
-      nodir: true,
-    });
+  async onChange(filename: FullPathFile, remove: boolean = false) {
+    let filepath = GodotPath.fromAbs(filename, this._godotProjectDir);
+
+    let scene = this.getScene(filepath);
+    if (!scene) {
+      await this.addScenes([filename], 1);
+      this.lastUpdate = [filepath.base];
+    }
+
+    let toUpdate = this._findDependants(filepath.base);
+    if (remove) {
+      const toRemove = toUpdate.delete(filepath.base);
+      // this.unloadScene(filepath);
+    }
+
+    let toUpdateFinale = [...toUpdate].map((m) =>
+      path.join(this._godotProjectDir, m)
+    );
+    await this.addScenes(toUpdateFinale);
+    this.lastUpdate = [...toUpdate];
+  }
+
+  getScene(gp: GodotPath): GodotScene | undefined {
+    return this.scenes.get(gp.base);
+  }
+
+  _findDependants(scenePath: string, acc?: Set<string>): Set<string> {
+    let res = acc ?? new Set();
+    res.add(scenePath);
+    for (let r of this._getDependencies(scenePath)) {
+      this._findDependants(r, res);
+    }
+    return res;
+  }
+
+  async _loadTscns(
+    files: string[],
+    nbWorker?: number
+  ): Promise<GodotScene[][]> {
     let chu = chunks(files, nbWorker || getWorkersNb(files.length));
-    return Promise.all(
+    return await Promise.all(
       chu.map(
         (i) =>
           new Promise<GodotScene[]>((resolve, reject) => {
@@ -64,8 +109,15 @@ export class GodotManager {
     );
   }
 
-  getScene(gp: GodotPath): GodotScene | undefined {
-    return this.scenes.get(gp.base);
+  _getDependencies(key: string): Set<string> {
+    return this.dependencies.get(key) || new Set();
+  }
+
+  _deleteDependencies(key: GodotPath) {
+    this.dependencies.delete(key.base);
+    for (const [k, v] of this.dependencies) {
+      v.delete(key.base);
+    }
   }
 
   _setDependency(child: GodotPath, parent: GodotPath) {
@@ -75,6 +127,10 @@ export class GodotManager {
 
   _addScene(scene: GodotScene) {
     this.scenes.set(scene.path.base, scene);
+  }
+
+  _deleteScene(scene: GodotPath) {
+    this.scenes.delete(scene.base);
   }
 }
 function chunks<T>(arr: T[], n: number): T[][] {
