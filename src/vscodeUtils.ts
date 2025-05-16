4bfaa407de143e27305da54c9f5de1e2d6ccf52c
jgirardet <ijkl@netc.fr>
2025-05-16 13:04:16 +0200

    fix newClass in panl asks for tscn path

--- src/vscodeUtils.ts
+++ src/vscodeUtils.ts
@@ -27,15 +27,17 @@ const getConfigValue = (key: string): string => {
 
 export const registerGCommand = (
   commandName: string,
-  command: (...args: any[]) => any,
-  ...args: any[]
+  command: (...args: any[]) => any
 ): Disposable => {
-  return commands.registerCommand(NAME + "." + commandName, async () => {
-    try {
-      await command(...args);
-    } catch (e) {
-      logger.error(e);
-      window.showErrorMessage(`Godot 4 Rust Error: ${e}`);
+  return commands.registerCommand(
+    NAME + "." + commandName,
+    async (...args: any[]) => {
+      try {
+        await command(...args);
+      } catch (e) {
+        logger.error(e);
+        window.showErrorMessage(`Godot 4 Rust Error: ${e}`);
+      }
     }
-  });
+  );
 };
