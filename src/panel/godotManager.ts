b41de3704265586f9a3dbd3122044b07cc6438a3
jgirardet <ijkl@netc.fr>
2025-05-16 12:23:55 +0200

    extraxt tscntree

--- src/panel/godotManager.ts
+++ src/panel/godotManager.ts
@@ -27,6 +27,7 @@ import { getGodotProjectDir } from "../utils";
 import { RustFiles, RustManager } from "../rust/rustmanager";
 import { GodotPath } from "../godot/godotPath";
 import path from "path";
+import { TscnTreeProvider } from "./tscnTreeData";
 
 export class GodotManager {
   treeView: TreeView<NodeItem>;
@@ -94,7 +95,6 @@ export class GodotManager {
     this.treeData.updateData(scenes, this.rust);
   }
 
-  // refresh = this.reload;
 
   async reload() {
     const scenes = await this.loader.reload();
@@ -136,90 +136,4 @@ export class GodotManager {
       focus: true, // good scroll position
     });
   }
-}
-
-class TscnTreeProvider implements TreeDataProvider<NodeItem> {
-  data: Map<string, NodeItem> = new Map();
-
-  treeChanged = new EventEmitter<
-    void | NodeItem | NodeItem[] | null | undefined
-  >();
-
-  constructor(private godotDir: FullPathDir) {}
-
-  onDidChangeTreeData:
-    | Event<void | NodeItem | NodeItem[] | null | undefined>
-    | undefined = this.treeChanged.event;
-
-  updateData(scenes: Map<string, GodotScene>, rust: RustManager) {
-    this.data.clear();
-
-    // initial load
-    for (const [k, s] of scenes.entries()) {
-      let root = NodeItem.createRoot(s);
-      let serchedStruct = rust.modules.get(s.rootNode.type!.value); //ok
-      if (serchedStruct) {
-        root.iconPath = NodeItem.getGodotRustIcon();
-        root.tooltip = serchedStruct.className;
-        root.rustModule = serchedStruct;
-      }
-      this.data.set(k, root);
-    }
-
-    // process packed scenes, afterwards
-    for (const [k, v] of this.data.entries()) {
-      let packed = v.getPackedSceneChildren();
-      for (const p of packed) {
-        let packedResPath = p.node.instance?.value.path.value;
-        if (packedResPath) {
-          let packedGPath = GodotPath.fromRes(packedResPath);
-          let rootNodeItem = this.data.get(packedGPath.base);
-          if (rootNodeItem) {
-            p.instanceType = rootNodeItem.type;
-            let serchedStruct = rust.modules.get(p.instanceType);
-            if (serchedStruct) {
-              p.iconPath = NodeItem.getGodotRustIcon();
-              p.tooltip = serchedStruct.baseClass;
-            } else {
-              p.tooltip = rootNodeItem.tooltip;
-              p.iconPath = rootNodeItem.iconPath;
-            }
-          }
-        }
-      }
-    }
-    this.treeChanged.fire();
-  }
-
-  getTreeItem(element: NodeItem): TreeItem | Thenable<TreeItem> {
-    return element;
-  }
-
-  refresh() {
-    this.treeChanged.fire();
-  }
-
-  async getChildren(
-    element?: NodeItem | undefined
-  ): Promise<NodeItem[] | null | undefined> {
-    if (!element) {
-      let roots = [...this.data.values()];
-      roots.sort((a, b) => (a.name > b.name ? 1 : -1));
-      return roots;
-    } else {
-      return element.children;
-    }
-  }
-
-  getParent(element: NodeItem): ProviderResult<NodeItem> {
-    return element.parent;
-  }
-
-  // resolveTreeItem?(
-  //   item: TreeItem,
-  //   element: NodeItem,
-  //   token: CancellationToken
-  // ): TreeItem | null | undefined {
-  //   throw new Error("Not implement resolveItem");
-  // }
 }
